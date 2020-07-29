4a62a23c9fa174f87020f44c1bcfcdd4d78802e9
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-07-29 16:59:05 -0300

    get absolute value

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ $initial = '555';
 //Find a PHP function to convert this octal string ($initial) to a decimal number.
 $a= octdec($initial);
 echo $a."\n";
+<?php
+$initial = '555';
+
+//Find a PHP function to convert this octal string ($initial) to a decimal number.
+$a= octdec($initial);
+echo $a."\n";
 
 //The value of $a is a number of degrees. Use a PHP function to convert it to radians and store the result in a new variable, $b.
 
@@ -25,5 +31,8 @@ echo $d."\n";
 $e = log($d);
 echo $e."\n";
 
+//take the absolute value of $e and store it in $f.
 
+$f = abs($e);
+echo $f."\n";
 
