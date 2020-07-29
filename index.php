f65d2dc7d9c166c2288fb169dd56aad6d6752c3e
Lorena Arabia <56326571+Larabia@users.noreply.github.com>
2020-07-29 16:46:52 -0300

    degrees to radians

--- index.php
+++ index.php
@@ -3,5 +3,10 @@ $initial = '555';
 
 //Find a PHP function to convert this octal string ($initial) to a decimal number.
 $a= octdec($initial);
-
 echo $a."\n";
+
+//The value of $a is a number of degrees. Use a PHP function to convert it to radians and store the result in a new variable, $b.
+
+$b = deg2rad($a);
+echo $b."\n";
+
